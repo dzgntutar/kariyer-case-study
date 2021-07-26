08490a3084682ef3b006269225b0ded8224796a1
dutarrr <duzguntutar@gmail.com>
2021-07-26 13:02:47 +0300

    job and company models created. mongo db connection complated

---
+++ app/routes/job.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+
+const Job = require("../models/Job");
+
+router.get("/", function (req, res, next) {
+  const firstJob = new Job({
+    name: "Kolera",
+  });
+
+  firstJob.save((error, data) => {
+    if (error) console.log(error);
+    res.json(data);
+  });
+});
+
+module.exports = router;
